2f76f8d452fee98977df0c1d1b51d603f29ee1be
denismarchesan <denismarchesan347@gmail.com>
2024-10-09 16:59:29 +0200

    casa

---
+++ data.js
@@ -0,0 +1,357 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20241007_181432_408-1",
+      "name": "IMG_20241007_181432_408 (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 3.027526619902133,
+        "pitch": -0.05137679668097306,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0283497416022307,
+          "pitch": -0.050553674980875485,
+          "rotation": 0,
+          "target": "1-img_20241007_181424_364"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_20241007_181424_364",
+      "name": "IMG_20241007_181424_364",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 2.5199457009240014,
+        "pitch": -0.020807691953391938,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.509854536462141,
+          "pitch": 0.2223967998086458,
+          "rotation": 0,
+          "target": "2-img_20241007_181414_262"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9049076576294564,
+          "pitch": 0.40080426609105757,
+          "title": "Livingroom",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-img_20241007_181414_262",
+      "name": "IMG_20241007_181414_262",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -2.362488862124689,
+        "pitch": -0.0184048682532314,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.290417156740478,
+          "pitch": 0.0017219874493168419,
+          "rotation": 0,
+          "target": "3-img_20241007_181406_146"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.6329532078504503,
+          "pitch": 0.1951442643794543,
+          "title": "Kitchen",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-img_20241007_181406_146",
+      "name": "IMG_20241007_181406_146",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 3.064161904076335,
+        "pitch": 0.17420594849969362,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04079010332250732,
+          "pitch": -0.03406769019575506,
+          "rotation": 0,
+          "target": "4-img_20241007_181356_932"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8895971392102915,
+          "pitch": 0.18102380273545826,
+          "title": "Bathroom",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-img_20241007_181356_932",
+      "name": "IMG_20241007_181356_932",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 3.0713569102233107,
+        "pitch": 0.14088047610149168,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.100363971471344,
+          "pitch": -0.36533055594178876,
+          "rotation": 0,
+          "target": "5-img_20241007_181347_420"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9024968252351986,
+          "pitch": -0.5292126166575564,
+          "title": "first floor",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-img_20241007_181347_420",
+      "name": "IMG_20241007_181347_420",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -0.5267216311187752,
+        "pitch": 0.28834088027534577,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3230327129263664,
+          "pitch": 0.3137255514968089,
+          "rotation": 0,
+          "target": "6-img_20241007_181338_295"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.17879798120038082,
+          "pitch": 0.14130499808768704,
+          "title": "Bathroom 1st floor",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-img_20241007_181338_295",
+      "name": "IMG_20241007_181338_295",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10564107942190759,
+          "pitch": 0.2164236707771252,
+          "rotation": 0,
+          "target": "7-img_20241007_181323_936"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7695289668431613,
+          "pitch": 0.24347584125776045,
+          "title": "Small bedroom",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-img_20241007_181323_936",
+      "name": "IMG_20241007_181323_936",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.0189452566561474,
+          "pitch": 0.27532171958713647,
+          "title": "main bedroom",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Casetta del Brolo",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
